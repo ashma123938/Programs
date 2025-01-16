ad4182f2189ce132c9ebd8859f22e3097519184e
ashma123938 <ashmashaikh1430@gmail.com>
2025-01-16 13:29:23 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,27 @@
+package Java;
+
+public class Leap {
+
+	public static void main(String[] args) {
+		int year = 2020;
+	    boolean isLeapYear;
+
+	    // divisible by 4
+	    isLeapYear = (year % 4 == 0);
+
+	    // divisible by 4, not by 100, or divisible by 400
+	    isLeapYear = isLeapYear && (year % 100 != 0 || year % 400 == 0);
+
+	    if (isLeapYear) {
+	      System.out.println(year + " is a leap year.");
+	    } else {
+	      System.out.println(year + " is not a leap year.");
+	    }
+	  }
+	
+		
+	}
+     //output:2020 is a leap year.
+
+    
+
---
+++ Voting.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class Voting {
+
+	public static void main(String[] args) {
+		int age=20;
+		if(age>18)
+			//if statement is true then print candidate is eligible for voting
+		System.out.println("candidate is eligible for voting");
+		else
+			//else statement is true then print candidate is  not eligible for voting
+			System.out.println("candidate is not eligible for voting");
+				
+				
+		
+	}
+
+}
+  //output:candidate is eligible for voting
+
+  
